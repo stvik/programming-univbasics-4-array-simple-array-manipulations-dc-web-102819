ac191f534754ffcaf1edb2e70bbf854158dea1c8
Sara Tarnvik <stvik2@gmail.com>
2019-10-09 19:46:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,15 @@ end
 def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_wiht_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
